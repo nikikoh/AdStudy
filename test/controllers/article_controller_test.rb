5bcf9a63e6e7d037bd9a2eed8f74a14c3eadb202
nikikoh <nikikoh5@gmail.com>
2021-03-25 22:25:34 +0900

    articleのコントローラーを作成しました

--- test/controllers/article_controller_test.rb
+++ test/controllers/article_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class ArticleControllerTest < ActionDispatch::IntegrationTest
+class ArticlesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get article_index_url
     assert_response :success
